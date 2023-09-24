5f6b2e36b6280a2aa07e9d3502d5f9dc6721cbee
Pavel Baldin <bpa25.07.2002@mail.ru>
2023-09-24 17:22:15 +0500

    decomposedFormModules

---
+++ src/assets/js/form/formSubmit.js
@@ -0,0 +1,25 @@
+import postRequest from '../PostRequest';
+import formValidate from './formValidate';
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+export default function handleSubmit(event, formFields) {
+    event.preventDefault();
+
+    const values = {};
+    for (let i = 0; i < formFields.length; i++) {
+        const element = formFields[i];
+        values[element.id] = element.value;
+    }
+
+    if (formValidate(values)) {
+        postRequest(url, values);
+    } else {
+        return;
+    }
+
+    /* eslint-disable no-param-reassign */
+    formFields.forEach((item) => {
+        item.value = '';
+    });
+}
