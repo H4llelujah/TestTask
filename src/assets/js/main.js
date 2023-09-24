ed6cfc309ac1573a5772df66d0fe219ce8de372f
Pavel Baldin <bpa25.07.2002@mail.ru>
2023-09-24 17:22:41 +0500

    addEventListenersAndSomeImport

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,28 @@
 import '../styles/css/styles.css';
+import formSubmit from './form/formSubmit';
+
+const button = document.querySelector('.feedback__button');
+
+const modal = document.querySelector('.modal');
+
+const modalContent = document.querySelector('.modal__content');
+
+const form = document.querySelector('.form');
+
+const inputsAndTextarea = form.querySelectorAll('input, textarea');
+
+function toogleModal() {
+    modal.classList.toggle('modal-active');
+}
+
+button.addEventListener('click', toogleModal);
+
+modalContent.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+modal.addEventListener('click', toogleModal);
+
+form.addEventListener('submit', (event) => {
+    formSubmit(event, inputsAndTextarea);
+});
